cad5055b2481a3a30a640023d6c424151c0c8566
Ortex <ortex@i.ua>
2018-03-28 12:59:07 +0000

    second C9

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 // In your Javascript (external .js resource or <script> tag)
 $(document).ready(function() {
-    $('.select-phone').select2();
+    $('.select-phone').select2({
+        templateResult: formatState
+    });
 
 });
+
+function formatState (state) {
+  if (!state.id) {
+    return state.text;
+  }
+  var baseUrl = "/img/flags";
+  var $state = $(
+    '<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.png" class="img-flag" /> ' + state.text + '</span>'
+  );
+  return $state;
+};
