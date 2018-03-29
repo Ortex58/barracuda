55c322b41dc17028d66981e67b23be56eddc63d1
Ortex <ortex@i.ua>
2018-03-29 21:38:21 +0000

    c9 finish

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
-$('.slider').slick({
+$(document).ready(function() {
+        $('.slider').slick({
         dots:true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         arrows: false
     });
+    
+        //Burger open, close
+        $(".sandwich, .menu_item").click(function() {
+          $(".sandwich").toggleClass("active");
+        });
+        
+        //show menu click
+        $(".header__menu__btn").click(function(){
+            $(".header__nav__nav2").slideToggle();
+           
+        });
+        
+        // Scroll UP
+        $(".footer__btnUp").click(function() {
+	        $('body,html').animate({scrollTop: top}, 2000);
+	});
+});
