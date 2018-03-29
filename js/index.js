f22c9fafd4fb5079442cb207ff9320b69027bf17
Breez <ortex@i.ua>
2018-03-29 10:59:15 +0300

    second PC

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+        dots:true,
+        fade: true,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 7000,
+        accessibility: false,
+        arrows: false
+    });
