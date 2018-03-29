eb3fdb2b33700ee7706ec3de82c745d30edcbcb0
Breez <ortex@i.ua>
2018-03-29 13:15:53 +0300

    second laptop

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
 $('.slider').slick({
         dots:true,
-        fade: true,
-        cssEase: 'linear',
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 7000,
-        accessibility: false,
+        autoplay: true,
         arrows: false
     });
